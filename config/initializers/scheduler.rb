383aca81b7af09b0d0a7feb8927604dd7bd8d1a6
Gahee Heo <ghbooth12@gmail.com>
2016-03-18 17:15:49 -0500

    ch10-rufus-scheduler-modify: completed

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,8 +5,8 @@ TodoApp::Application.load_tasks
 load File.join(Rails.root, 'lib', 'tasks', 'todo.rake')
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1m' do  # run every minutes
-# scheduler.cron '0 0 * * *' do   # run every midnight
+# scheduler.every '1m' do  # run every minutes
+scheduler.cron '0 0 * * *' do   # run every midnight
   Rake::Task["todo:delete_items"].reenable
   Rake::Task["todo:delete_items"].invoke
 end
