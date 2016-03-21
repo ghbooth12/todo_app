bdde946e88526a48bd6d821d4acfce6199521457
Gahee Heo <ghbooth12@gmail.com>
2016-03-21 13:05:10 -0500

    ch10-rufus-scheduler-modify2: edited 'every day'

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,7 +6,8 @@ load File.join(Rails.root, 'lib', 'tasks', 'todo.rake')
 scheduler = Rufus::Scheduler.new
 
 # scheduler.every '1m' do  # run every minutes
-scheduler.cron '0 0 * * *' do   # run every midnight
+# scheduler.cron '0 0 * * * America/Chicago' do  # cron works only local
+scheduler.every '1d' do   # run everyday
   Rake::Task["todo:delete_items"].reenable
   Rake::Task["todo:delete_items"].invoke
 end
